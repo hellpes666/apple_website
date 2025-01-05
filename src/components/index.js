2ebc47ec0c2710ee1923696a4511764377e7a9a6
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-01-05 20:31:03 +0300

    add something on website about apple :) enjoy it

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Highlights from "./Highlights";
+import VideoCarousel from "./VideoCarousel";
+
+export {Navbar, Hero, Highlights, VideoCarousel}
