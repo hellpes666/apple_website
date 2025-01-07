47e93090ec8c4bb6d2546d988622bf25319de198
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-01-07 15:34:24 +0300

    remove Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: 'https://32d0f48ef6b4ffc8b444a22425961510@o4508602328875008.ingest.de.sentry.io/4508602342834256',
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
